3e4fb78a393878199e7f7d7d35e658ce73111142
KimYoungJun <cyberact3181@gmail.com>
2025-01-09 17:29:36 +0900

    feat: request dto record로 변경

--- MemberService/src/main/java/com/example/memberservice/api/request/UserRegisterRequest.java
+++ MemberService/src/main/java/com/example/memberservice/api/request/UserRegisterRequest.java
@@ -2,8 +2,6 @@ package com.example.memberservice.api.request;
 
 import lombok.Getter;
 
-@Getter
-public class UserRegisterRequest {
-    private String loginId;
-    private String userName;
+
+public record UserRegisterRequest(String loginId, String userName) {
 }
