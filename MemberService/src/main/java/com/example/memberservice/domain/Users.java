6298126cdc6525463e5c341db7d01bd334cc1cb9
KimYoungJun <cyberact3181@gmail.com>
2025-01-09 16:59:22 +0900

    feat: user도메인, 엔티티 생성

---
+++ MemberService/src/main/java/com/example/memberservice/domain/Users.java
@@ -0,0 +1,31 @@
+package com.example.memberservice.domain;
+
+import com.example.memberservice.infrastructure.entity.UserEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Users {
+    private Long id;
+    private String loginId;
+    private String userName;
+
+    public static Users from(UserEntity userEntity) {
+        return Users.builder()
+                .id(userEntity.getId())
+                .loginId(userEntity.getLoginId())
+                .userName(userEntity.getUserName())
+                .build();
+    }
+
+    public static Users of(String loginId, String userName) {
+        return Users.builder()
+                .loginId(loginId)
+                .userName(userName)
+                .build();
+    }
+}
---
+++ MemberService/src/main/java/com/example/memberservice/infrastructure/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package com.example.memberservice.infrastructure.entity;
+
+import com.example.memberservice.domain.Users;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String loginId;
+
+    private String userName;
+
+    public static UserEntity toEntity(Users user) {
+        return UserEntity.builder()
+                .id(user.getId())
+                .loginId(user.getLoginId())
+                .userName(user.getUserName())
+                .build();
+    }
+}
